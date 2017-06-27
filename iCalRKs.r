af56121ad03c299aebae21d856996b98d9dfd353
byaxb <xinbo.ai@gmail.com>
2017-06-27 13:41:08 +0800

    Update iCalRKs.r

--- iCalRKs.r
+++ iCalRKs.r
@@ -47,7 +47,36 @@ load("inet.rda")
 #For the Hens
 #to calculate different centralities
 #make sure that abbreviated names are correct
-#
+# DEall: Degree (all)
+# DEin: Degree (in)
+# DEout: Degree (out)
+# COall: Coreness (all)
+# COin: Coreness (in)
+# COout: Coreness (out)
+# HIall: H-index(all)
+# HIin: H-index (in)
+# HIout: H-index (out)
+# ECall: Eccentricity (all)
+# ECin: Eccentricity (in)
+# ECout: Eccentricity (out)
+# CLall: Closeness (all)
+# CLin: Closeness (in)
+# CLout: Closeness (out)
+# IN: Information index
+# BE: Betweenness centrality
+# LO: Load centrality
+# ST: Stress centrality
+# SU: Subgraph centrality
+# EI: Eigenvector centrality
+# AL: Alpha centrality
+# PR: Page Rank
+# AU: HITs (Authority)
+# HU: HITs (Hub)
+# ExFs: Expected Force (ExF)
+# EnVbtw: Entropy Variation (btw)
+# EnVall: Entropy Variation (all)
+# EnVout: Entropy Variation(out)
+# EnVin: Entropy Variation(in)
 HenCentralities <- iCalCen(Hens,c("DEall","DEin","DEout",
                                   "COall","COin","COout",
                                   "HIall","HIin","HIout",
